33f1ba2a2b2c5365d5be5a407da9340b9d8166d0
Carlo Beltrame <beltrame@puzzle.ch>
2018-03-09 17:31:30 +0100

    Fix typos and remove debug code

--- fix-logic-midi.py
+++ fix-logic-midi.py
@@ -20,29 +20,14 @@ def fix_zero_length_notes(mid, maxmsg=10):
             if not(msg.type == 'note_off' and msg.time == 0):
                 out.append(msg)
                 continue
-            #print('-----')
             prev = out[-1]
-            #print(prev)
-            #print(msg)
-            #print('swap')
-            # swap times
+            # swap times saved on messages
             msg.time, prev.time = prev.time, msg.time
-            # insert before previous
+            # insert current before previous in list
             out[-1] = msg
             out.append(prev)
-            #print(msg)
-            #print(prev)
         # replace fixed track
         mid.tracks[t] = MidiTrack(out)
-
-def print_midi_file(mid, maxmsg=10):
-    for track in mid.tracks:
-        num = 0
-        for msg in track:
-            num += 1
-            if num > maxmsg and maxmsg > 0:
-                break
-            print(msg)
 
 mid = MidiFile('input.mid')
 fix_zero_length_notes(mid, 0)
